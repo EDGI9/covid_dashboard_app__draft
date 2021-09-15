c36b7f0adc32b422272894718db8cf3c83c6e2a7
vsilva <vitalinosilva@gmail.com>
2021-09-15 21:03:26 +0100

    added bootstrap and bootstrap vue

--- src/main.js
+++ src/main.js
@@ -3,7 +3,19 @@ import Store from './store';
 import Router from './router';
 import Resources from './resources';
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
+
 import App from './App.vue';
+
+
+Vue.use(Loading);
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 Vue.config.productionTip = false;
 
