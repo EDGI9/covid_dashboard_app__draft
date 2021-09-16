7466b8b38440cf22def4c5e6dbed624219d34cba
vsilva <vitalinosilva@gmail.com>
2021-09-16 09:25:41 +0100

    updated added logic to fetch and process all countries names

--- src/services/modules/countryService.js
+++ src/services/modules/countryService.js
@@ -2,6 +2,26 @@ import Common from '../../common';
 
 function processCountryData(dataPayload, entitiesPayload)
 {
+    const result = [];
+
+    if(dataPayload && Common.arrayCommon.isValid(dataPayload) && entitiesPayload)
+    {
+        dataPayload.forEach( (item, index )=> {
+
+            const tempEntity = entitiesPayload.countryEntity();
+            tempEntity.id = (index + 1);
+            tempEntity.name = item.Country;
+            tempEntity.iso = item.ISO2;
+                       
+            result.push(tempEntity);
+        });
+    }
+
+    return result;
+}
+
+function processCountriesTotalData(dataPayload, entitiesPayload)
+{
     let result = {};
 
     if(dataPayload && Common.arrayCommon.isValid(dataPayload) && entitiesPayload)
@@ -11,9 +31,11 @@ function processCountryData(dataPayload, entitiesPayload)
         tempEntity.name = dataPayload[0].Country;
         tempEntity.statusDescription = dataPayload[0].Status;
         
-        dataPayload.forEach( item => {
+        dataPayload.forEach( (item, index ) => {
 
-            const tempDateCaseEntity = entitiesPayload.dateCaseEntity( Common.momentCommon.formatUtcDate(item.Date), item.Cases );
+            tempEntity.id = (index + 1);
+
+            const tempDateCaseEntity = entitiesPayload.dateCaseEntity( (index + 1) ,Common.momentCommon.formatUtcDate(item.Date), item.Cases );
             tempEntity.statuses.push( tempDateCaseEntity );
             
         });
@@ -25,7 +47,8 @@ function processCountryData(dataPayload, entitiesPayload)
 }
 
 const _service_module = {
-    processCountryData
+    processCountryData,
+    processCountriesTotalData
 };
 
 export default _service_module;
