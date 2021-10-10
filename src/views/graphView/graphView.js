84e3385144e370afcead6ce1f7bc3b5a8b795a60
Edgar Dias <dias.s.edgar@gmail.com>
2021-10-10 18:45:10 +0100

    Adding filter component to graph page and allow to filter graph data

--- src/views/graphView/graphView.js
+++ src/views/graphView/graphView.js
@@ -1,41 +1,60 @@
 import { mapGetters } from 'vuex';
+import FilterComponent from '../../components/filterComponent';
 import Common from '../../common';
+import Utils from '../../utils';
 import ChartComponent from '../../components/chartComponent';
 
+const _countryStore = "countryStore/";
 const _graphStore = "graphStore/";
+const _summaryStore = "summaryStore/";
 
 export default {
   name: 'graph-view',  
   components:{
-    'chart-component': ChartComponent
+    'chart-component': ChartComponent,
+    'filter-component': FilterComponent
   },
   computed:{
     ...mapGetters({
-      graphData: _graphStore + 'GRAPH_DATA'
+      graphData: _graphStore + 'GRAPH_DATA',
+      totalSummary: _summaryStore + 'TOTAL_SUMMARY',
+      countries: _countryStore + 'COUNTRIES',
     }),
-  },created(){
-    this.init(this.graphData);
+  },
+  created(){
+    this.setChartData(this.graphData);
   },
   data()
   {
     return{
-      chartData: []
+      chartData: [],
     };
   }, 
   methods:{
-    init(graphDataPayload){
-      this.chartData = ( Common.arrayCommon.isValid(graphDataPayload.data) ) ? graphDataPayload: {};
+    setChartData(chartDataPayload){
+      this.chartData = ( Common.arrayCommon.isValid(chartDataPayload.data) ) ? chartDataPayload: {};
     },
+    //TODO: Create a Mixin for the capitalize
     captilizeTitle(titlePayload)
     {
       return (titlePayload && titlePayload === 'summary')? Common.stringCommon.capitalizeFirstLetter(titlePayload) : titlePayload;
+    },
+    //TODO: Move these data filtering and chart update methods to a common/utils file
+    filterData(filterRequestPayload)
+    { 
+      const results = Utils.filterUtils.filterObjectByNameProperty(this.totalSummary, filterRequestPayload.searchedCountry);
+      this.updateChartData(results);
+    },
+    updateChartData(dataPayload)
+    {
+      Utils.graphUtils.updateGraphData(this, _graphStore + 'SET_GRAPH_DATA', 'summary', dataPayload);
     }
   },
   watch:{
     graphData:{
       handler(updatedValuePayload)
       {
-        this.init(updatedValuePayload);
+        this.setChartData(updatedValuePayload);
       },
     },
     deep: true
