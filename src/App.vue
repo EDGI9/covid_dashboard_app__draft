687d88bb0857c6ac683380b9001657e0a53cf6f2
Edgar Dias <dias.s.edgar@gmail.com>
2021-11-08 19:51:38 +0000

    Changing some colors and content layouts

--- src/App.vue
+++ src/App.vue
@@ -60,6 +60,7 @@ export default {
 </script>
 
 <style lang="scss">
+@import "~bootstrap/scss/bootstrap-utilities";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -68,6 +69,7 @@ export default {
   color: #2c3e50;
   position: relative;
   min-height: 100vh;  
+  background-color: $blue-100;
 
   #content-wrap{
     padding-bottom: 156px;    /* Footer height */
